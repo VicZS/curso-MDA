e85329c75147a6303047ae83c37cbdf431193771
VicZS <97258881+VicZS@users.noreply.github.com>
2024-04-11 21:09:57 -0600

    Add files via upload

--- Transporte Público/Código Proyecto/js/mapa_inicio.js
+++ Transporte Público/Código Proyecto/js/mapa_inicio.js
@@ -1,9 +1,14 @@
+//Funcion que permite inicializa un mapa y poder visualizarlo
 function iniciarMap(){
+    //señala las coordenadas donde iniciara el mapa
     var coord = {lat:19.0016629 ,lng: -98.199963};
+    //creacion de un elemento de mapa con las coordenadas y el zoom que deseemos tener
+    //al momento de visualizarlo
     var map = new google.maps.Map(document.getElementById('map'),{
         zoom: 15,
         center: coord
     });
+    //Agrega un marcador en las coordenadas dadas y en el mapa usado
     var marker = new google.maps.Marker({
         position: coord,
         map: map
